d772bfda4480781448806b1a6794bd0a5d5df345
boonej2 <jboone1998@live.com>
2025-11-10 22:40:26 -0500

    Revert "fix"
    This reverts commit a37a192d8f03e3d7bccef6e2dda03a4702e0cff4.

--- next.config.js
+++ next.config.js
@@ -17,20 +17,6 @@ const nextConfig = {
     ],
     formats: ['image/webp', 'image/avif'],
   },
-  outputFileTracingIncludes: {
-    '/api/**/*': [
-      './generated/prisma/**/*',
-    ],
-  },
-  experimental: {
-    outputFileTracingExcludes: {
-      '*': [
-        'node_modules/@swc/core-linux-x64-gnu',
-        'node_modules/@swc/core-linux-x64-musl',
-        'node_modules/@esbuild/linux-x64',
-      ],
-    },
-  },
 }
 
 module.exports = nextConfig
